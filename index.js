9480e2d04902e592f7f55c0fd04431e7571f7f61
Sam W <samevwro@gmail.com>
2025-04-28 12:23:51 -0600

    First Commit

---
+++ index.js
@@ -0,0 +1,150 @@
+class Player{
+    constructor(name){
+        this.name = name;
+        this.playerCards = [];
+        this.selectedCard = [];
+        this.points = 0;
+    }
+}
+
+class Cards {
+    constructor(){
+        this.cards = ['Ace',2,3,4,5,6,7,8,9,10,'Jack','Queen','King'];
+        this.suit = ['♣️','♠️','❤️','🍀'];
+        this.deck = [];
+        this.splitDeck1 = [];
+        this.splitDeck2 = []
+        
+    };
+    createDeck(){
+        for(var x = 0; x < this.suit.length ; x++){
+            for(let i = 0; i < this.cards.length ; i++){
+                this.deck.push(`${this.suit[x]}${this.cards[i]}`);
+                }
+        }return(this.deck);
+        
+    };
+    splitDeck() {
+        for( let i = 0; i < 26; i++){
+            let randomArrayNum = (Math.floor(Math.random() * this.deck.length));
+            this.splitDeck1.push(this.deck[randomArrayNum]);
+            this.deck.splice(randomArrayNum, 1);;
+        };
+        for( let i = 0; i < 26; i++){
+            let randomArrayNum = (Math.floor(Math.random() * this.deck.length));
+            this.splitDeck2.push(this.deck[randomArrayNum]);
+            this.deck.splice(randomArrayNum, 1);;
+        };
+    }; 
+};
+
+class Menu{
+    constructor(){
+        this.players = [];
+        this.numOfWar = 0;
+    }
+    start() {
+        console.log('Welcome to the War card game.\n The game will start now!!');
+        var player1 = new Player();
+        this.players.push(player1);
+        var player2 = new Player();
+        this.players.push(player2);
+        var newDeck = new Cards();
+        newDeck.createDeck();
+        newDeck.splitDeck();
+        player1.playerCards = player1.playerCards.concat(newDeck.splitDeck1);
+        player2.playerCards = player2.playerCards.concat(newDeck.splitDeck2);
+        
+        
+        while(Object.keys(player1.playerCards).length > 0 && Object.keys(player2.playerCards).length > 0) {
+            
+            player1.selectedCard.push(player1.playerCards[0]);
+            player2.selectedCard.push(player2.playerCards[0]);
+            player1.playerCards.splice(0, 1) 
+            player1.playerCards.splice(0, 1)
+            var cardNum1 = this.findKings(player1.selectedCard)
+            var cardNum2 = this.findKings(player2.selectedCard)
+            
+            console.log(`Player 1's card is (${player1.selectedCard.join('')}): Player 2's card is (${player2.selectedCard})`)
+            if(cardNum1 > cardNum2) {
+                player1.playerCards.push(player1.selectedCard[0]);
+                player1.playerCards.push(player2.selectedCard[0]);
+                
+                console.log('Player 1 has the larger card and wins this round!');
+                player1.points += 1
+                player1.selectedCard.splice(0, 1)
+                player2.selectedCard.splice(0, 1)
+            }
+            else if(cardNum1 < cardNum2) {
+                player2.playerCards.push(player1.selectedCard[0]);
+                player2.playerCards.push(player2.selectedCard[0]);
+                console.log('Player 2 has the larger card and wins this round!');
+                player2.points += 1
+                player1.selectedCard.splice(0, 1)
+                player2.selectedCard.splice(0, 1)
+            }
+            else if(cardNum1 == cardNum2) {
+                console.log('This is WAR!!!')
+                let p1War = player1.playerCards.splice(0, 3);
+                let p2War = player2.playerCards.splice(0, 3);
+                let warWinner = p1War.concat(p2War);
+                if(this.findKings(p1War) > this.findKings(p2War)){
+                    console.log(`Player 1 draws (${p1War[0]}): Player 2 draws (${p2War[0]}). \nPlayer 1 wins`);
+                    player1.playerCards = player1.playerCards.concat(warWinner);
+                    player1.points += 1
+                }else if(this.findKings(p1War) < this.findKings(p2War)){
+                    console.log(`Player 1 draws (${p1War[0]}): Player 2 draws (${p2War[0]}). \nPlayer 2 wins`);
+                    player2.playerCards = player2.playerCards.concat(warWinner);
+                    player2.points += 1
+                }else {
+                    console.log(`Player 1 draws (${p1War[0]}): Player 2 draws (${p2War[0]}). \nIt's a tie. Draw again`);
+                    player1.selectedCard.push(p1War);
+                    player2.selectedCard.push(p2War);
+                    
+                }p1War.splice(0, 3)
+                p2War.splice(0, 3)
+            }
+            player1.selectedCard.splice(0, 1)
+            player2.selectedCard.splice(0, 1)
+            console.log('Player 1: ',player1.points)
+            console.log('Player 2: ',player2.points)
+            console.log(Object.keys(player1.playerCards).length)
+            console.log(Object.keys(player2.playerCards).length)
+            if(player1.points > 100 || player2.points > 100){
+                console.log(`Player 1 Score: ${player1.points}. Player 2 Score: ${player2.points}`);
+                break;
+            }
+            
+            console.log('---------------End Of Round----------------\n')
+            
+            setTimeout(() => {
+                
+            }, 50000);
+        }
+        
+
+        
+    }
+    
+    findKings(z) {
+        const myJSON = JSON.stringify(z)
+        let card = myJSON.split("")
+        if(card[4] == 'K'){
+            return(13)
+        }else if(card[4] == 'Q'){
+            return(12)
+        }else if(card[4] == 'A'){
+            return(1)
+        }else if(card[4] == 'J'){
+            return(11)
+        }else if (parseInt(card[4]) == 1) {
+            return(10)
+        }else{
+            return(parseInt(card[4]))
+        }
+    }
+};
+
+let menu = new Menu;
+menu.start()
+
