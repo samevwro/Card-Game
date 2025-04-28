8a7969e34a1e2150880e45a900c36a63111897b3
Sam <samevwro@gmail.com>
2025-04-28 15:14:14 -0600

    laptop

--- index.js
+++ index.js
@@ -56,12 +56,12 @@ class Menu{
         player2.playerCards = player2.playerCards.concat(newDeck.splitDeck2);
         
         
-        while(Object.keys(player1.playerCards).length > 0 && Object.keys(player2.playerCards).length > 0) {
+        while(Object.keys(player1.playerCards).length >= 1 && Object.keys(player2.playerCards).length >= 1) {
             
-            player1.selectedCard.push(player1.playerCards[0]);
-            player2.selectedCard.push(player2.playerCards[0]);
-            player1.playerCards.splice(0, 1) 
-            player1.playerCards.splice(0, 1)
+            //player1.selectedCard.push(player1.playerCards[0]);
+            //player2.selectedCard.push(player2.playerCards[0]);
+            player1.selectedCard = player1.playerCards.splice(0, 1) 
+            player2.selectedCard = player2.playerCards.splice(0, 1)
             var cardNum1 = this.findKings(player1.selectedCard)
             var cardNum2 = this.findKings(player2.selectedCard)
             
@@ -85,32 +85,44 @@ class Menu{
             }
             else if(cardNum1 == cardNum2) {
                 console.log('This is WAR!!!')
-                let p1War = player1.playerCards.splice(0, 3);
-                let p2War = player2.playerCards.splice(0, 3);
+                this.numOfWar += 1
+                player1.playerCards.splice(2, 0, player1.selectedCard);
+                player2.playerCards.splice(2, 0, player2.selectedCard);
+                console.log(player1.playerCards)
+                let p1War = player1.playerCards.splice(0, 4);
+                let p2War = player2.playerCards.splice(0, 4);
                 let warWinner = p1War.concat(p2War);
+                console.log(player1.playerCards)
                 if(this.findKings(p1War) > this.findKings(p2War)){
-                    console.log(`Player 1 draws (${p1War[0]}): Player 2 draws (${p2War[0]}). \nPlayer 1 wins`);
+                    console.log(`Player 1 draws: (${p1War[0]}), Player 2 draws: (${p2War[0]}). \nPlayer 1 wins`);
                     player1.playerCards = player1.playerCards.concat(warWinner);
                     player1.points += 1
                 }else if(this.findKings(p1War) < this.findKings(p2War)){
-                    console.log(`Player 1 draws (${p1War[0]}): Player 2 draws (${p2War[0]}). \nPlayer 2 wins`);
+                    console.log(`Player 1 draws: (${p1War[0]}), Player 2 draws: (${p2War[0]}). \nPlayer 2 wins`);
                     player2.playerCards = player2.playerCards.concat(warWinner);
                     player2.points += 1
                 }else {
-                    console.log(`Player 1 draws (${p1War[0]}): Player 2 draws (${p2War[0]}). \nIt's a tie. Draw again`);
-                    player1.selectedCard.push(p1War);
-                    player2.selectedCard.push(p2War);
+                    console.log(`Player 1 draws: (${p1War[0]}), Player 2 draws: (${p2War[0]}). \nIt's a tie. Draw again`);
+                    player1.playerCards.push(p1War);
+                    player2.playerCards.push(p2War);
                     
                 }p1War.splice(0, 3)
                 p2War.splice(0, 3)
+                
             }
             player1.selectedCard.splice(0, 1)
             player2.selectedCard.splice(0, 1)
+            player1.selectedCard.pop
+            player2.selectedCard.pop
+            
             console.log('Player 1: ',player1.points)
             console.log('Player 2: ',player2.points)
+            console.log(`War total is: ${this.numOfWar}`)
+            console.log(player1.playerCards)
+            console.log(player2.playerCards)
             console.log(Object.keys(player1.playerCards).length)
             console.log(Object.keys(player2.playerCards).length)
-            if(player1.points > 100 || player2.points > 100){
+            if(player1.points > 10 || player2.points > 10){
                 console.log(`Player 1 Score: ${player1.points}. Player 2 Score: ${player2.points}`);
                 break;
             }
@@ -134,7 +146,7 @@ class Menu{
         }else if(card[4] == 'Q'){
             return(12)
         }else if(card[4] == 'A'){
-            return(1)
+            return(14)
         }else if(card[4] == 'J'){
             return(11)
         }else if (parseInt(card[4]) == 1) {
@@ -147,4 +159,3 @@ class Menu{
 
 let menu = new Menu;
 menu.start()
-
